fd693276fdf8097ab3556b43062ee52a1e939bf8
florient2016 <Koffieza71@gmail.com>
2025-08-20 19:49:38 +0200

    Initial commit of rosa-terraform project

--- update-sandbox/data.tf
+++ update-sandbox/data.tf
@@ -1,4 +1,4 @@
-# data.tf
+# data.tf - Updated to get ROSA account role policies
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
@@ -7,7 +7,13 @@ data "rhcs_versions" "rosa_versions" {
   search = "enabled='t' and rosa_enabled='t' and channel_group='stable'"
 }
 
-# Get ROSA policies - this gives us the correct Red Hat AWS account IDs!
+# Get ROSA account role policies for the specific version
+data "rhcs_rosa_operator_roles" "operator_roles" {
+  operator_role_prefix = var.prefix
+  account_role_prefix  = var.prefix
+}
+
+# Get the exact policies ROSA expects
 data "rhcs_policies" "account_role_policies" {}
 
 locals {
@@ -18,9 +24,11 @@ locals {
   red_hat_aws_account = "710019948333"  # Official Red Hat AWS account
   
   common_tags = {
-    Environment = "rosa-sts"
-    ManagedBy   = "terraform"
-    Purpose     = "rosa-cluster"
-    Prefix      = var.prefix
+    Environment                  = "rosa-sts"
+    ManagedBy                   = "terraform"
+    Purpose                     = "rosa-cluster"
+    Prefix                      = var.prefix
+    "red-hat-managed"           = "true"
+    "rosa.openshift.io/version" = var.openshift_version
   }
 }
--- update-sandbox/rosa-sts-roles.tf
+++ update-sandbox/rosa-sts-roles.tf
@@ -1,14 +1,17 @@
-# rosa-sts-roles.tf - Updated with OpenShift version in role names
+# rosa-sts-roles.tf - Updated with AWS managed policies for ROSA 4.16
 
 # Get the latest available OpenShift version if not specified
 locals {
   # Extract major.minor version for role naming (e.g., "4.16" from "4.16.0")
   openshift_major_minor = join(".", slice(split(".", var.openshift_version), 0, 2))
   
-  # Account role definitions with OpenShift version
+  # Account role definitions with correct AWS managed policies
   account_roles = {
     installer = {
       name = "ManagedOpenShift-Installer-Role"
+      managed_policies = [
+        "arn:aws:iam::aws:policy/service-role/ROSAInstallerPolicy"
+      ]
       trust_policy_statements = [{
         effect = "Allow"
         principals = {
@@ -25,6 +28,9 @@ locals {
     }
     support = {
       name = "ManagedOpenShift-Support-Role"
+      managed_policies = [
+        "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
+      ]
       trust_policy_statements = [{
         effect = "Allow"
         principals = {
@@ -41,6 +47,9 @@ locals {
     }
     controlplane = {
       name = "ManagedOpenShift-ControlPlane-Role"
+      managed_policies = [
+        "arn:aws:iam::aws:policy/service-role/ROSAControlPlanePolicy"
+      ]
       trust_policy_statements = [{
         effect = "Allow"
         principals = {
@@ -52,6 +61,9 @@ locals {
     }
     worker = {
       name = "ManagedOpenShift-Worker-Role"
+      managed_policies = [
+        "arn:aws:iam::aws:policy/service-role/ROSAWorkerInstancePolicy"
+      ]
       trust_policy_statements = [{
         effect = "Allow"
         principals = {
@@ -105,252 +117,22 @@ resource "aws_iam_role" "account_roles" {
   max_session_duration = 3600
   
   tags = merge(local.common_tags, {
-    Name              = "${var.prefix}-${local.openshift_major_minor}-${each.value.name}"
-    Type              = "AccountRole"
-    Role              = each.key
-    OpenShiftVersion  = var.openshift_version
-    OpenShiftMajorMinor = local.openshift_major_minor
+    Name                     = "${var.prefix}-${local.openshift_major_minor}-${each.value.name}"
+    Type                     = "AccountRole"
+    Role                     = each.key
+    OpenShiftVersion         = var.openshift_version
+    OpenShiftMajorMinor      = local.openshift_major_minor
+    "red-hat-managed"        = "true"
+    "rosa.openshift.io/version" = var.openshift_version
   })
 }
 
-# Custom policy for Installer Role
-data "aws_iam_policy_document" "installer_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      # EC2 permissions
-      "ec2:AllocateAddress",
-      "ec2:AssociateAddress",
-      "ec2:AttachInternetGateway",
-      "ec2:AttachNetworkInterface",
-      "ec2:AuthorizeSecurityGroupEgress",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CopyImage",
-      "ec2:CreateImage",
-      "ec2:CreateInstanceProfile",
-      "ec2:CreateKeyPair",
-      "ec2:CreateNatGateway",
-      "ec2:CreateNetworkInterface",
-      "ec2:CreateRoute",
-      "ec2:CreateRouteTable",
-      "ec2:CreateSecurityGroup",
-      "ec2:CreateSubnet",
-      "ec2:CreateTags",
-      "ec2:CreateVpc",
-      "ec2:CreateVpcEndpoint",
-      "ec2:CreateInternetGateway",
-      "ec2:DeleteInternetGateway",
-      "ec2:DeleteKeyPair",
-      "ec2:DeleteNatGateway",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DeleteRoute",
-      "ec2:DeleteRouteTable",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DeleteSnapshot",
-      "ec2:DeleteSubnet",
-      "ec2:DeleteTags",
-      "ec2:DeleteVpc",
-      "ec2:DeleteVpcEndpoint",
-      "ec2:DeregisterImage",
-      "ec2:DescribeAccountAttributes",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeImages",
-      "ec2:DescribeInstanceAttribute",
-      "ec2:DescribeInstanceCreditSpecifications",
-      "ec2:DescribeInstances",
-      "ec2:DescribeInstanceTypes",
-      "ec2:DescribeInternetGateways",
-      "ec2:DescribeKeyPairs",
-      "ec2:DescribeNatGateways",
-      "ec2:DescribeNetworkAcls",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribeRegions",
-      "ec2:DescribeRouteTables",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSnapshots",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeTags",
-      "ec2:DescribeVolumes",
-      "ec2:DescribeVpcAttribute",
-      "ec2:DescribeVpcClassicLink",
-      "ec2:DescribeVpcClassicLinkDnsSupport",
-      "ec2:DescribeVpcEndpoints",
-      "ec2:DescribeVpcs",
-      "ec2:DetachInternetGateway",
-      "ec2:DetachNetworkInterface",
-      "ec2:DisassociateAddress",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:ModifyNetworkInterfaceAttribute",
-      "ec2:ModifySubnetAttribute",
-      "ec2:ModifyVpcAttribute",
-      "ec2:ReleaseAddress",
-      "ec2:RevokeSecurityGroupEgress",
-      "ec2:RevokeSecurityGroupIngress",
-      "ec2:RunInstances",
-      "ec2:TerminateInstances",
-      
-      # IAM permissions
-      "iam:AddRoleToInstanceProfile",
-      "iam:AttachRolePolicy",
-      "iam:CreateInstanceProfile",
-      "iam:CreateRole",
-      "iam:DeleteInstanceProfile",
-      "iam:DeleteRole",
-      "iam:DeleteRolePolicy",
-      "iam:DetachRolePolicy",
-      "iam:GetInstanceProfile",
-      "iam:GetRole",
-      "iam:GetRolePolicy",
-      "iam:ListInstanceProfilesForRole",
-      "iam:ListRolePolicies",
-      "iam:PassRole",
-      "iam:PutRolePolicy",
-      "iam:RemoveRoleFromInstanceProfile",
-      "iam:TagInstanceProfile",
-      "iam:TagRole",
-      "iam:UntagRole",
-      
-      # Route53 permissions
-      "route53:ChangeResourceRecordSets",
-      "route53:CreateHostedZone",
-      "route53:DeleteHostedZone",
-      "route53:GetChange",
-      "route53:GetHostedZone",
-      "route53:ListHostedZones",
-      "route53:ListHostedZonesByName",
-      "route53:ListResourceRecordSets",
-      "route53:ListTagsForResource",
-      "route53:UpdateHostedZoneComment",
-      
-      # S3 permissions
-      "s3:CreateBucket",
-      "s3:DeleteBucket",
-      "s3:DeleteObject",
-      "s3:GetBucketAcl",
-      "s3:GetBucketTagging",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:ListBucket",
-      "s3:PutBucketAcl",
-      "s3:PutBucketTagging",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      
-      # ELB permissions
-      "elasticloadbalancing:AddTags",
-      "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-      "elasticloadbalancing:AttachLoadBalancerToSubnets",
-      "elasticloadbalancing:ConfigureHealthCheck",
-      "elasticloadbalancing:CreateListener",
-      "elasticloadbalancing:CreateLoadBalancer",
-      "elasticloadbalancing:CreateTargetGroup",
-      "elasticloadbalancing:DeleteListener",
-      "elasticloadbalancing:DeleteLoadBalancer",
-      "elasticloadbalancing:DeleteTargetGroup",
-      "elasticloadbalancing:DeregisterTargets",
-      "elasticloadbalancing:DescribeListeners",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticloadbalancing:DescribeTargetGroups",
-      "elasticloadbalancing:DescribeTargetHealth",
-      "elasticloadbalancing:ModifyListener",
-      "elasticloadbalancing:ModifyLoadBalancerAttributes",
-      "elasticloadbalancing:ModifyTargetGroup",
-      "elasticloadbalancing:RegisterTargets",
-      "elasticloadbalancing:RemoveTags",
-      "elasticloadbalancing:SetSecurityGroups",
-      "elasticloadbalancing:SetSubnets"
-    ]
-    resources = ["*"]
-  }
-}
-
-# Support role policy
-data "aws_iam_policy_document" "support_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "sts:AssumeRole"
-    ]
-    resources = ["*"]
-  }
-}
-
-# Control plane role policy
-data "aws_iam_policy_document" "controlplane_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "ec2:AttachVolume",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CreateSecurityGroup",
-      "ec2:CreateTags",
-      "ec2:CreateVolume",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DeleteVolume",
-      "ec2:Describe*",
-      "ec2:DetachVolume",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:ModifyVolume",
-      "ec2:RevokeSecurityGroupIngress",
-      "elasticloadbalancing:AddTags",
-      "elasticloadbalancing:AttachLoadBalancerToSubnets",
-      "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-      "elasticloadbalancing:CreateListener",
-      "elasticloadbalancing:CreateLoadBalancer",
-      "elasticloadbalancing:CreateTargetGroup",
-      "elasticloadbalancing:ConfigureHealthCheck",
-      "elasticloadbalancing:DeleteListener",
-      "elasticloadbalancing:DeleteLoadBalancer",
-      "elasticloadbalancing:DeleteTargetGroup",
-      "elasticloadbalancing:Describe*",
-      "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-      "elasticloadbalancing:ModifyListener",
-      "elasticloadbalancing:ModifyLoadBalancerAttributes",
-      "elasticloadbalancing:ModifyTargetGroup",
-      "elasticloadbalancing:RegisterTargets",
-      "elasticloadbalancing:RemoveTags",
-      "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-      "elasticloadbalancing:SetSubnets",
-      "kms:DescribeKey"
-    ]
-    resources = ["*"]
-  }
-}
-
-# Worker role policy
-data "aws_iam_policy_document" "worker_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "ec2:DescribeInstances",
-      "ec2:DescribeRegions",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:GetRepositoryPolicy",
-      "ecr:DescribeRepositories",
-      "ecr:ListImages",
-      "ecr:DescribeImages",
-      "ecr:BatchGetImage"
-    ]
-    resources = ["*"]
-  }
-}
-
-# Apply custom policies to account roles
-resource "aws_iam_role_policy" "account_role_policies" {
+# Attach AWS managed policies to roles
+resource "aws_iam_role_policy_attachment" "account_role_policies" {
   for_each = local.account_roles
   
-  name = "${var.prefix}-${local.openshift_major_minor}-${each.key}-policy"
-  role = aws_iam_role.account_roles[each.key].id
-  
-  policy = each.key == "installer" ? data.aws_iam_policy_document.installer_policy.json : (
-    each.key == "support" ? data.aws_iam_policy_document.support_policy.json : (
-      each.key == "controlplane" ? data.aws_iam_policy_document.controlplane_policy.json : 
-      data.aws_iam_policy_document.worker_policy.json
-    )
-  )
+  role       = aws_iam_role.account_roles[each.key].name
+  policy_arn = each.value.managed_policies[0]
 }
 
 # Create instance profiles for EC2 roles with OpenShift version
@@ -360,10 +142,12 @@ resource "aws_iam_instance_profile" "controlplane_instance_profile" {
   path = var.path
   
   tags = merge(local.common_tags, {
-    Name                = "${var.prefix}-${local.openshift_major_minor}-controlplane-instance-profile"
-    Type                = "InstanceProfile"
-    OpenShiftVersion    = var.openshift_version
-    OpenShiftMajorMinor = local.openshift_major_minor
+    Name                         = "${var.prefix}-${local.openshift_major_minor}-controlplane-instance-profile"
+    Type                         = "InstanceProfile"
+    OpenShiftVersion             = var.openshift_version
+    OpenShiftMajorMinor          = local.openshift_major_minor
+    "red-hat-managed"            = "true"
+    "rosa.openshift.io/version"  = var.openshift_version
   })
 }
 
@@ -373,9 +157,11 @@ resource "aws_iam_instance_profile" "worker_instance_profile" {
   path = var.path
   
   tags = merge(local.common_tags, {
-    Name                = "${var.prefix}-${local.openshift_major_minor}-worker-instance-profile"
-    Type                = "InstanceProfile"
-    OpenShiftVersion    = var.openshift_version
-    OpenShiftMajorMinor = local.openshift_major_minor
+    Name                         = "${var.prefix}-${local.openshift_major_minor}-worker-instance-profile"
+    Type                         = "InstanceProfile"
+    OpenShiftVersion             = var.openshift_version
+    OpenShiftMajorMinor          = local.openshift_major_minor
+    "red-hat-managed"            = "true"
+    "rosa.openshift.io/version"  = var.openshift_version
   })
 }

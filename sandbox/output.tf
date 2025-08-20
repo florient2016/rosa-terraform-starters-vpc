ca4842ebf447d39bdbfa8254e0a1bfc56aa3c6ef
florient2016 <Koffieza71@gmail.com>
2025-08-20 17:05:26 +0200

    Initial commit of rosa-terraform project

--- sandbox/output.tf
+++ sandbox/output.tf
@@ -1,85 +1,76 @@
 # outputs.tf
-
-# Module-based outputs
+# Method 1: RHCS Resources Outputs
 output "rosa_ocm_role_arn" {
   description = "ARN of the OCM role for ROSA cluster creation"
-  value       = try(module.account_iam_resources.rosa_ocm_role_arn, aws_iam_role.ocm_role.arn)
+  value       = try(rhcs_rosa_ocm_role.ocm_role.arn, aws_iam_role.ocm_role.arn)
 }
 
 output "rosa_user_role_arn" {
   description = "ARN of the User role for ROSA cluster creation"
-  value       = try(module.account_iam_resources.rosa_user_role_arn, aws_iam_role.user_role.arn)
+  value       = try(rhcs_rosa_user_role.user_role.arn, aws_iam_role.user_role.arn)
 }
 
 output "rosa_installer_role_arn" {
   description = "ARN of the Installer role"
-  value       = try(
-    module.account_iam_resources.rosa_installer_role_arn,
-    aws_iam_role.account_roles["Installer-Role"].arn
-  )
+  value       = try(rhcs_rosa_account_roles.account_roles.installer_role_arn, aws_iam_role.account_roles["installer"].arn)
 }
 
 output "rosa_support_role_arn" {
   description = "ARN of the Support role"
-  value       = try(
-    module.account_iam_resources.rosa_support_role_arn,
-    aws_iam_role.account_roles["Support-Role"].arn
-  )
+  value       = try(rhcs_rosa_account_roles.account_roles.support_role_arn, aws_iam_role.account_roles["support"].arn)
 }
 
 output "rosa_controlplane_role_arn" {
   description = "ARN of the Control Plane role"
-  value       = try(
-    module.account_iam_resources.rosa_controlplane_role_arn,
-    aws_iam_role.account_roles["ControlPlane-Role"].arn
-  )
+  value       = try(rhcs_rosa_account_roles.account_roles.controlplane_role_arn, aws_iam_role.account_roles["controlplane"].arn)
 }
 
 output "rosa_worker_role_arn" {
-  description = "ARN of the Worker role"
-  value       = try(
-    module.account_iam_resources.rosa_worker_role_arn,
-    aws_iam_role.account_roles["Worker-Role"].arn
-  )
+  description = "ARN of the Worker role"  
+  value       = try(rhcs_rosa_account_roles.account_roles.worker_role_arn, aws_iam_role.account_roles["worker"].arn)
 }
 
-# Operator roles (if created)
-output "operator_role_arns" {
-  description = "ARNs of all operator roles"
-  value       = try(module.operator_iam_resources.operator_role_arns, {})
-}
-
-# OIDC Configuration
 output "oidc_config_id" {
   description = "ID of the OIDC configuration"
-  value       = local.oidc_config_id
+  value       = try(rhcs_rosa_oidc_config.oidc_config.id, aws_iam_openid_connect_provider.rosa_oidc.arn)
 }
 
-output "oidc_config_issuer_url" {
-  description = "Issuer URL of the OIDC configuration"
-  value       = try(rhcs_rosa_oidc_config.oidc_config[0].oidc_endpoint_url, "")
+output "operator_role_arns" {
+  description = "ARNs of all operator roles"
+  value       = try(rhcs_rosa_operator_roles.operator_roles.operator_role_arns, {})
 }
 
-# Account information
 output "aws_account_id" {
   description = "AWS account ID where roles are created"
   value       = local.account_id
 }
 
-output "aws_partition" {
-  description = "AWS partition"
-  value       = local.partition
-}
-
-# All role ARNs for easy consumption
 output "all_role_arns" {
   description = "Map of all ROSA STS role ARNs"
   value = {
-    ocm_role        = try(module.account_iam_resources.rosa_ocm_role_arn, aws_iam_role.ocm_role.arn)
-    user_role       = try(module.account_iam_resources.rosa_user_role_arn, aws_iam_role.user_role.arn)
-    installer_role  = try(module.account_iam_resources.rosa_installer_role_arn, aws_iam_role.account_roles["Installer-Role"].arn)
-    support_role    = try(module.account_iam_resources.rosa_support_role_arn, aws_iam_role.account_roles["Support-Role"].arn)
-    controlplane_role = try(module.account_iam_resources.rosa_controlplane_role_arn, aws_iam_role.account_roles["ControlPlane-Role"].arn)
-    worker_role     = try(module.account_iam_resources.rosa_worker_role_arn, aws_iam_role.account_roles["Worker-Role"].arn)
+    ocm_role          = try(rhcs_rosa_ocm_role.ocm_role.arn, aws_iam_role.ocm_role.arn)
+    user_role         = try(rhcs_rosa_user_role.user_role.arn, aws_iam_role.user_role.arn)
+    installer_role    = try(rhcs_rosa_account_roles.account_roles.installer_role_arn, aws_iam_role.account_roles["installer"].arn)
+    support_role      = try(rhcs_rosa_account_roles.account_roles.support_role_arn, aws_iam_role.account_roles["support"].arn)
+    controlplane_role = try(rhcs_rosa_account_roles.account_roles.controlplane_role_arn, aws_iam_role.account_roles["controlplane"].arn)
+    worker_role       = try(rhcs_rosa_account_roles.account_roles.worker_role_arn, aws_iam_role.account_roles["worker"].arn)
   }
+}
+
+# CLI commands equivalent output
+output "rosa_create_cluster_command" {
+  description = "ROSA CLI command to create cluster using these roles"
+  value = <<-EOT
+    rosa create cluster \
+      --cluster-name ${var.prefix}-cluster \
+      --sts \
+      --role-arn ${try(rhcs_rosa_account_roles.account_roles.installer_role_arn, aws_iam_role.account_roles["installer"].arn)} \
+      --support-role-arn ${try(rhcs_rosa_account_roles.account_roles.support_role_arn, aws_iam_role.account_roles["support"].arn)} \
+      --controlplane-iam-role ${try(rhcs_rosa_account_roles.account_roles.controlplane_role_arn, aws_iam_role.account_roles["controlplane"].arn)} \
+      --worker-iam-role ${try(rhcs_rosa_account_roles.account_roles.worker_role_arn, aws_iam_role.account_roles["worker"].arn)} \
+      --region ${var.aws_region} \
+      --version ${var.openshift_version} \
+      --compute-machine-type m5.xlarge \
+      --replicas 3
+  EOT
 }
